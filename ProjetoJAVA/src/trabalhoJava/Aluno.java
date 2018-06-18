8abde81f288a10ce06fd29babb73e93af042179b
Alexandre T. Soriani <40251083+alesoriani@users.noreply.github.com>
2018-06-18 17:39:17 -0300

    novo arquivo aluno_correção
    corrigindo Aluno

--- ProjetoJAVA/src/trabalhoJava/Aluno.java
+++ ProjetoJAVA/src/trabalhoJava/Aluno.java
@@ -7,33 +7,57 @@ public class Aluno {
 
     public Aluno(Integer idA, String nomeA, String sobrenomeA) {
         this.nomeAluno = nomeA;
-        this.sobrenomeAluno= sobrenomeA;
+        this.sobrenomeAluno = sobrenomeA;
         this.idAluno = idA;
     }
 
-    public Aluno juntaNome(String nomeAluno, String sobrenomeAluno) {
-        return nomeAluno + sobrenomeAluno + idAluno;
+    public String getNome() {
+        return nomeAluno;
     }
 
-    @java.lang.Override
-    public java.lang.String toString() {
-        return "Aluno{" +
-                "nomeAluno='" + nomeAluno + '\'' +
-                ", sobrenomeAluno='" + sobrenomeAluno + '\'' +
-                ", idAluno=" + idAluno +
-                '}';
+    public void setNome(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
     }
 
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        if (!super.equals(object)) return false;
-        Aluno aluno = (Aluno) object;
-        return idAluno == aluno.idAluno;
+    public String getSobrenomeA() {
+        return sobrenomeAluno;
     }
 
+    public void setSobrenomeAluno(String sobrenomeA) {
+        this.sobrenomeAluno = sobrenomeAluno;
+    }
+
+    public Integer getidA() {
+        return idAluno;
+    }
+
+    public void setidA(Integer idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    @Override
     public int hashCode() {
-
-        return java.util.Objects.hash(super.hashCode(), idAluno);
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+        return result;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Aluno other = (Aluno) obj;
+        if (idAluno == null) {
+            if (other.idAluno != null)
+                return false;
+        } else if (!idAluno.equals(other.idAluno))
+            return false;
+        return true;
+    }
+
 }
