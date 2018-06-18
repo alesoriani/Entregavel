989683bce54bc307dd928c5aef90aad86d204127
Alexandre T. Soriani <40251083+alesoriani@users.noreply.github.com>
2018-06-18 17:42:56 -0300

    Correção da logica do Curso
    Corrigindo a logica da Classe Curso

--- ProjetoJAVA/src/trabalhoJava/Curso.java
+++ ProjetoJAVA/src/trabalhoJava/Curso.java
@@ -1,36 +1,85 @@
-package trabalhoJava;
-
+import java.util.ArrayList;
 import java.util.List;
 
-
 public class Curso {
-    public String nomeCurso;
-    private interger idCurso;
 
-    //Parte E - Prof Titular
-    public professorAdjunto;
-    public professorTitular;
-    public Interger maxAlunos;
-    public (List<Aluno>) matriculados;
+    public String curso;
+    public Integer codigo;
+    public ProfessorTitular professorTitular;
+    public ProfessorAdjunto professorAdjunto;
+    public Integer qtdeMaxAlunos;
+    public List<Aluno> listaAlunos;
 
-
-    public Curso(String nomeCurso) {
-        this.nomeCurso = nomeCurso;
+    public Curso(String curso, Integer codigo, ProfessorTitular professorTitular, ProfessorAdjunto professorAdjunto, Integer qtdeMaxAlunos, List<Aluno> listaAlunos) {
+        this.curso = curso;
+        this.codigo = codigo;
+        this.professorTitular = professorTitular;
+        this.professorAdjunto = professorAdjunto;
+        this.qtdeMaxAlunos = qtdeMaxAlunos;
+        this.listaAlunos = listaAlunos;
     }
 
-    @java.lang.Override
-    public java.lang.String toString() {
-        return "Curso{" +
-                "nomeCurso='" + nomeCurso + '\'' +
-                ", idCurso=" + idCurso +
-                '}';
+    public List<Aluno> getListaAlunos() {
+        return listaAlunos;
     }
 
-    public String getNomeCurso() {
-        return nomeCurso;
+    public void setListaAlunos(List<Aluno> listaAlunos) {
+        this.listaAlunos = listaAlunos;
     }
 
-    public void setNomeCurso(String nomeCurso) {
-        this.nomeCurso = nomeCurso;
+    public Integer getQtdeMaxAlunos() {
+        return qtdeMaxAlunos;
     }
+
+    public void setQtdeMaxAlunos(Integer qtdeMaxAlunos) {
+        this.qtdeMaxAlunos = qtdeMaxAlunos;
+    }
+
+    public ProfessorTitular getProfessorTitular() {
+        return professorTitular;
+    }
+
+    public void setProfessorTitular(ProfessorTitular professorTitular) {
+        this.professorTitular = professorTitular;
+    }
+
+    public ProfessorAdjunto getProfessorAdjunto() {
+        return professorAdjunto;
+    }
+
+    public void setProfessorAdjunto(ProfessorAdjunto professorAdjunto) {
+        this.professorAdjunto = professorAdjunto;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+
+    public Boolean adicionarUmAluno(Aluno umAluno) {
+        Integer qtdDeAlunosNoCurso = this.getListaAlunos().size();
+        if(qtdDeAlunosNoCurso < qtdeMaxAlunos) {
+            this.getListaAlunos().add(umAluno);
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void excluirAluno(Aluno umAluno) {
+        this.getListaAlunos().remove(umAluno);
+    }
+
 }
